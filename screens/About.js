177dcd8b547a1bf29f8d39d788affaf9f77516aa
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2023-10-24 12:05:44 +0300

    Update About.js

--- screens/About.js
+++ screens/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <View style={styles.container}>
         <Text style={styles.title}>About this app</Text>
           <Text style={styles.description}>
-            This app is designed to help users verify the trustworthiness of email addresses, phone numbers, and links.
+            This app is designed to help users to verify the trustworthiness of email addresses, phone numbers, and links.
             It uses the IPQualityScore API to perform these checks. The app provides a simple interface to enter the data and
             get the trustworthiness results instantly. The user can input an email address and check if it's valid and not abusive,
             enter a phone number to verify its authenticity, and also check links to detect suspicious or malicious URLs.
